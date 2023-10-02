5680256e0890377d79e6ee8095770dd4377a855d
Jaeyong Kim <ajfnwodyd3@naver.com>
2023-10-02 19:11:38 +0900

    RecyclerView and Glide (now image error)

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -3,17 +3,21 @@ package com.example.newsapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapp.databinding.ActivityMainBinding
 import com.tickaroo.tikxml.TikXml
 import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
+import org.jsoup.Jsoup
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var newsAdapter : NewsAdapter
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://news.google.com/")
         .addConverterFactory(
@@ -31,10 +35,47 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        newsAdapter = NewsAdapter()
+
+        binding.newsRecyclerView.apply{
+            layoutManager=LinearLayoutManager(context)
+            adapter = newsAdapter
+        }
+
+
         val newsService = retrofit.create(NewsService::class.java)
         newsService.mainFeed().enqueue(object: Callback<NewsRss>{
             override fun onResponse(call: Call<NewsRss>, response: Response<NewsRss>) {
                 Log.e("MainActivity","${response.body()?.channel?.items}")
+
+                val list = response.body()?.channel?.items.orEmpty().transform()
+                newsAdapter.submitList(list)
+
+                list.forEach{
+
+                    Thread{
+                        try{
+                            val jsoup = Jsoup.connect(it.link).get()
+                            val elements = jsoup.select("meta[property^=og:]")
+                            val ogImageNode = elements.find {node ->
+                                node.attr("property") == "og:image"
+                            }
+
+                            it.imageUrl=ogImageNode?.attr("content")
+                            Log.e("MainActivity","imageUrl :${it.imageUrl}")
+
+                        } catch (e: Exception){
+                            e.printStackTrace()
+                        }
+
+
+
+                    }.start()
+
+                }
+
+
+
             }
 
             override fun onFailure(call: Call<NewsRss>, t: Throwable) {
---
+++ app/src/main/java/com/example/newsapp/NewsAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.newsapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.newsapp.databinding.ItemNewsBinding
+
+class NewsAdapter : ListAdapter<NewsModel, NewsAdapter.ViewHolder>(diffUtil) {
+
+
+    inner class ViewHolder(private val binding: ItemNewsBinding): RecyclerView.ViewHolder(binding.root){
+
+        fun bind(item: NewsModel){
+            binding.titleTextView.text = item.title
+
+            Glide.with(binding.thumbnailImageView)
+                .load(item.imageUrl)//이미지 로드
+                .into(binding.thumbnailImageView) // 사용 . 타겟은 이미지뷰
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+       return ViewHolder(
+           ItemNewsBinding.inflate(
+               LayoutInflater.from(parent.context),parent,false
+           )
+       )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+
+    companion object{
+        val diffUtil = object: DiffUtil.ItemCallback<NewsModel>(){
+            override fun areItemsTheSame(oldItem: NewsModel, newItem: NewsModel): Boolean {
+                return oldItem === newItem
+            }
+
+            override fun areContentsTheSame(oldItem: NewsModel, newItem: NewsModel): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+}
